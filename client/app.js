945a071fc1d30f0f7718ca304a4a032dd0a175e1
jamie <level7jamie@gmail.com>
2024-10-22 11:36:45 +0100

    Ok for real this time...

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 const baseUrl = "https://monorepo-bw63.onrender.com";
 
 async function handleServerConnect() {
-  const response = await fetch(`${baseUrl}/`);
+  const response = await fetch(`${baseUrl}`);
   const data = await response.json();
   console.log(data);
 }
